413c2778178624076f07171d19f183243b0a2938
Salma Hassanin <salmahassanein77@gmail.com>
2025-10-26 12:15:10 +0300

    test4

--- .gitignore.swift
+++ .gitignore.swift
@@ -4,5 +4,10 @@
 //
 //  Created by Mohammed Hassanien on 26/10/2025.
 //
+ShopApp/Features/Home/Model/AppConfig.swift
+ShopApp.xcodeproj/xcshareddata/xcschemes/ShopApp.xcscheme
+.fuse_hidden*
+DerivedData/
+xcuserdata/ 
 # Ignore Xcode scheme with secrets
 ShopApp.xcodeproj/xcshareddata/xcschemes/*.xcscheme
