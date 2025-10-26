f16d35e16f44d81d207d614ed63741ba14bc9d27
Salma Hassanin <salmahassanein77@gmail.com>
2025-10-26 12:07:30 +0300

    test3

---
+++ .gitignore.swift
@@ -0,0 +1,8 @@
+//
+//  .gitignore.swift
+//  ShopApp
+//
+//  Created by Mohammed Hassanien on 26/10/2025.
+//
+# Ignore Xcode scheme with secrets
+ShopApp.xcodeproj/xcshareddata/xcschemes/*.xcscheme
